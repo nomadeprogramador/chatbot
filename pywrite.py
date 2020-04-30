3ac158c1f5d0b74ab7baa3e5ad09977037af3872
nomadeprogramador <64232807+nomadeprogramador@users.noreply.github.com>
2020-04-29 22:06:38 -0300

    Chat

---
+++ pywrite.py
@@ -0,0 +1,24 @@
+#by programador.nomade 
+#Follow on instragram 
+# video https://www.youtube.com/channel/UC3K_mQtRNLYcbKP6NnzcMTQ
+import pyautogui
+import time
+# you have to click on the chat to write 
+pyautogui.alert(text='BOT DO @programador.nomade')
+time.sleep(2)
+
+pyautogui.write('Eae gado',interval=0.1) # The first msg ! 
+pyautogui.press('Enter')
+for i in range(1,10): # loop 
+    pyautogui.write('Cade as mina manin  kkk') # write !!!  
+    pyautogui.press('Enter')
+    pyautogui.write('Gadin')# write 
+    for  i in range(1,10):# loop
+        pyautogui.press('Enter')
+        pyautogui.write('Aproveita ndo a quarentena para pastar man ?') #write 
+        pyautogui.write('Gadao') # write 
+    for x in range(1,10): #loop 
+        pyautogui.write('gado') # write 
+        pyautogui.press('Enter')
+                
+    
